0a012de0c95f66c11d309bc58df974d156bc44aa
Kekus_Gigabyte <user@WIN-G3Q8CVB0RMM>
2026-02-15 14:50:29 +0100

    initial commit
    Signed-off-by: Kekus_Gigabyte <user@WIN-G3Q8CVB0RMM>

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DndCharacterCreator.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
